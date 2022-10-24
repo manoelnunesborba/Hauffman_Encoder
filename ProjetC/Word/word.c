badf741002b7cc51266e30bf99ee8b4d1f12bccd
NUNES BORBA Manoel <manoel.nunesborba@yahoo.com>
2022-10-24 20:46:19 +0200

    Ceci n'est pas un bon message de commit : La lecture du fichier se fait correctement en utilisant la methode de jacques. Par contre j'ai pas réussi a utiliser la séparation des fichiers headers, Mais le code fait bien ce qu'il est sensé faire

--- ProjetC/Word/word.c
+++ ProjetC/Word/word.c
@@ -1,80 +1,12 @@
+//
+// Stolen by manoel jacqu on 21/10/2022.
+//
+
+#include "word/word.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include "Word/word.h"
-// code style:
-// Variable and function as camelCase:
-// ex : firstName lastName hello thisIsQuiteGood
-// Struct as PascalCase :
-// ex: FirstName LastName Hello ThisIsQuiteGood
 
-#define MAX_PATH 300
-typedef char String[MAX_PATH];
-typedef enum { false = 0, true = 1 } Booleen;
-Booleen debugOn = false;
-
-typedef struct {
-	char key;
-    int frequency; 
-
-}HashMap[5];
-
-void readInputString(String word){
-    scanf("%s", word);
-    if (debugOn) printf(">>echo %s\n", word);
-}
-Booleen hashMapHasValue(int size, char value, HashMap* output){
-    for(int i =0; i<size; i++){
-        if(output[i]->key == value)
-            return true;
-        if(debugOn)    printf("With. %u \n",output[i]->key);
-
-    }
-    return false;
-}
-
-int main() {
-    String buffer;
-    FILE *readFilePtr;
-    int fileSize;
-    char* fileBytes = NULL;
-    HashMap bufferMap;
-    printf("Hauffman encoder !\n");
-    printf("Enter file name: ");
-    readInputString(buffer);
-    while(access(buffer, F_OK) != 0)
-    {
-        printf("\n File entered was not valid, try another :");
-        readInputString(buffer);
-    };
-
-    if (fopen(buffer,"rb") != NULL) {
-        FILE* readFilePtr= fopen(buffer,"rb");
-        Start * start = createStart();
-        fseek(readFilePtr, 0L, SEEK_END); 
-        const long int res = ftell(readFilePtr); 
-        fileBytes =(char*)malloc(res * sizeof( char));
-        fseek(readFilePtr, 0L, SEEK_SET);
-        fread(fileBytes,res,1,readFilePtr); 
-
-        findLetterFrequency(start->start, fileBytes);
-        sortLinkedListWord(start->start);
-        /*fseek(readFilePtr, 0L, SEEK_END); 
-        fileBytes =(char*)malloc(res * sizeof( char));
-        fseek(readFilePtr, 0L, SEEK_SET);
-        fread(fileBytes,res,1,readFilePtr); 
-        frequencyFunction(res,fileBytes, &bufferMap);*/
-        printf("\n");
-
-    }else{
-        printf("Error");
-        return 1;
-    }
-
-    return 0;
-
-}
 Start * createStart(){
     Start * start = malloc(sizeof(start));
     start->start = NULL;
---
+++ ProjetC/Word/word.h
@@ -0,0 +1,27 @@
+//
+// Stolen by manoel on 21/10/2022.
+//
+
+#ifndef TEST5_WORD_H
+#define TEST5_WORD_H
+
+typedef struct LinkedListWord LinkedListWord;
+struct LinkedListWord {
+    char letter;
+    int frequency;
+    LinkedListWord *next;
+};
+
+typedef struct Start Start;
+struct Start {
+    LinkedListWord * start;
+};
+
+Start * createStart();
+void incrementFrequency(LinkedListWord * list, char letter);
+void addElement(LinkedListWord * list, char letter);
+int alrdyAdded(LinkedListWord * list, char letter);
+void findLetterFrequency(LinkedListWord * list, char * word);
+void sortLinkedListWord(LinkedListWord * list);
+
+#endif //TEST5_WORD_H
