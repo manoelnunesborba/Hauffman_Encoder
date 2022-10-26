dd134c329a63b779e2ea4f3dd1654383b8f4175b
Manoel NUNES BORBA <76746287+manoelnunesborba@users.noreply.github.com>
2022-10-26 11:58:58 +0200

    Delete(Unused functions)

--- ProjetC/main.c
+++ ProjetC/main.c
@@ -15,92 +15,20 @@
 typedef char String[MAX_PATH];
 typedef enum { false = 0, true = 1 } Booleen;
 Booleen debugOn = false;
-typedef struct LinkedListWord LinkedListWord;
-struct LinkedListWord {
-    char letter;
-    double frequency;
-    LinkedListWord *next;
-};
-
-typedef struct Start Start;
-struct Start {
-    LinkedListWord * start;
-};
 
 
 
-void readInputString(String word);
-
-Booleen hashMapHasValue(int size, char value, HashMap* output);
-
-int isNULL(LinkedListWord * list);
-
-void createHuffmanTree(LinkedListRoot * linkedListRoot, HuffmanRoot * root);
-void createBranch(LinkedListRoot * listWord, HuffmanRoot * root);
-void initHuffmanTree(LinkedListRoot * linkedListRoot, HuffmanRoot * root);
+void readInputString(String word){
+    scanf("%s", word);
+    if (debugOn) printf(">>echo %s\n", word);
+}
 
 void displayDocumentation();
 char * displayInterface();
 void commandReader(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
-
-    char testWord[MAX_WORD_LENGTH] = "bcaadddccacacac";
-    //char testWord2[MAX_WORD_LENGTH] = "AAABBBCCCO POKPOKPOKAAADADADDDDD \n AAABBBCCCO POKPOKPOKAAADADADDDDD";
-
-    // Creation table des frequence
-
-    LinkedListRoot * linkedListRoot = createRoot();
-    findLetterFrequency(linkedListRoot->start, testWord);
-    sortLinkedListWord(linkedListRoot->start);
-
-    // Creation arbre de huffman
-    HuffmanRoot * Huffmanroot = createHuffmanRoot();
-    createHuffmanTree(linkedListRoot, Huffmanroot);
-
-    /*String buffer;
-    FILE *readFilePtr;
-    int fileSize;
-    char* fileBytes = NULL;
-    printf("Hauffman encoder !\n");
-    printf("Enter file name: ");
-    readInputString(buffer);
-    while(access(buffer, F_OK) != 0)
-    {
-        printf("\n File entered was not valid, try another :");
-        readInputString(buffer);
-    };
-
-    if (fopen(buffer,"rb") != -1) {
-        //setvbuf(stdout, NULL, _IONBF, 0);
-        printf("Yo");
-        FILE* readFilePtr= fopen(buffer,"rb");
-        LinkedListRoot * start = createRoot();
-        fseek(readFilePtr, 0L, SEEK_END); 
-        const long int res = ftell(readFilePtr); 
-        fileBytes =(char*)malloc(res * sizeof( char));
-        fseek(readFilePtr, 0L, SEEK_SET);
-        fread(fileBytes,res,1,readFilePtr); 
-
-
-        findLetterFrequency(start->start, fileBytes);
-        sortLinkedListWord(start->start);
-        *//*fseek(readFilePtr, 0L, SEEK_END);
-        fileBytes =(char*)malloc(res * sizeof( char));
-        fseek(readFilePtr, 0L, SEEK_SET);
-        fread(fileBytes,res,1,readFilePtr); 
-        frequencyFunction(res,fileBytes, &bufferMap);*//*
-        //Next code goes hire
-        printf("\n");
-
-    }else{
-        printf("Error");
-        return 1;
-    }*/
-
     return 0;
-
-
 }
 
 void commandReader(int argc, char *argv[]) {
@@ -114,6 +42,7 @@ void commandReader(int argc, char *argv[]) {
     } else {
         if(strcmp(argv[1], "-f") == 0 || strcmp(argv[1], "--fast") == 0){
             printf("Compression rapide du fichier %s vers %s\n", argv[2], argv[3]);
+
         } else if(strcmp(argv[1], "-s") == 0 || strcmp(argv[1], "--slow") == 0){
             printf("Compression lente du fichier %s vers %s\n", argv[2], argv[3]);
         } else if(strcmp(argv[1], "-d") == 0 || strcmp(argv[1], "--decompress") == 0){
@@ -197,84 +126,5 @@ char * displayInterface(){
             printf("Error : invalid choice \n");
             break;
     }
-}
-
-
-void readInputString(String word){
-    scanf("%s", word);
-    if (debugOn) printf(">>echo %s\n", word);
-}
-
-Booleen hashMapHasValue(int size, char value, HashMap* output){
-    for(int i =0; i<size; i++){
-        if(output[i]->key == value)
-            return true;
-        if(debugOn)    printf("With. %u \n",output[i]->key);
-
-    }
-    return false;
-}
-
-int isNULL(LinkedListWord * list) {
-    return list == NULL ? 1 : 0;
-}
-
-void initHuffmanTree(LinkedListRoot * linkedListRoot, HuffmanRoot * root) {
-    root->Root = createHuffmanLeaf(' ',0);
-
-    LinkedListWord * listWord = linkedListRoot->start;
-
-    if(isNULL(linkedListRoot->start) || isNULL(linkedListRoot->start->next))
-        return;
-
-    HuffmanLeaf * leafLeft = NULL;
-    HuffmanLeaf * leafRight = NULL;
-
-    if(listWord->frequency < listWord->next->frequency) {
-        leafLeft = createHuffmanLeaf(listWord->letter, listWord->frequency);
-        leafRight = createHuffmanLeaf(listWord->next->letter, listWord->next->frequency);
-    } else {
-        leafLeft = createHuffmanLeaf(listWord->next->letter, listWord->next->frequency);
-        leafRight = createHuffmanLeaf(listWord->letter, listWord->frequency);
-    }
-    addLeaves(root->Root, leafLeft, leafRight);
-    linkedListRoot->start = linkedListRoot->start->next->next;
-    addStack(linkedListRoot->start, root->Root->Frequency);
-}
-
-void createHuffmanTree(LinkedListRoot * linkedListRoot, HuffmanRoot * root) {
-    if(root->Root == NULL){
-        initHuffmanTree(linkedListRoot,root);
-    }
-    while(linkedListRoot->start != NULL ) {
-        createBranch(linkedListRoot, root);
-    }
-}
-
-void createBranch(LinkedListRoot * listWordRoot, HuffmanRoot * root) {
-    LinkedListWord * listWord = listWordRoot->start;
-    if(isNULL(listWord) || isNULL(listWord->next)) {
-        listWord == NULL;
-    }
-    int nextLetterFrequency = listWord->letter == NULL ? listWord->next->frequency : listWord->frequency;
-    char nextLetter = listWord->letter == NULL ? listWord->next->letter : listWord->letter;
-    HuffmanLeaf * newParent = createHuffmanLeaf(' ', 0);
-    if(nextLetterFrequency < root->Root->Frequency) {
-        HuffmanLeaf * left = createHuffmanLeaf(nextLetter, nextLetterFrequency);
-        HuffmanLeaf * right = root->Root;
-        addLeaves(newParent,left,right);
-    } else {
-        HuffmanLeaf * left = root->Root;
-        HuffmanLeaf * right = createHuffmanLeaf(nextLetter, nextLetterFrequency);
-        addLeaves(newParent,left,right);
-    }
-    root->Root = newParent;
-    if(isNULL(listWord->letter) && isNULL(listWord->next->next->next))
-        listWordRoot->start = listWordRoot->start->next->next->next;
-    listWordRoot->start = listWordRoot->start->next->next;
-    if(listWordRoot->start == NULL)
-        return;
-    addStack(listWordRoot->start,root->Root->Frequency);
-    sortLinkedListWord(listWordRoot->start);
 }
 
