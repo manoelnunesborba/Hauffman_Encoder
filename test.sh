6f4e87ac2eb330f8c960a9e73e963f8e2a3ad3f0
Lucas <contact@lucasquitman.fr>
2022-11-19 01:51:38 +0100

    improving tests script and adding more tests

--- test.sh
+++ test.sh
@@ -10,14 +10,29 @@ _help() {
    echo "-f     test the fast compression"
    echo "-i     provide an input file, defaults to the bible"
    echo "-k     keep files after execution"
+   echo "-t     target compression percentage to pass, defaults to 10"
    echo
 }
+_cleanup() {
+    if [[ $keep_file == 'yes' ]]; then
+        exit $exit_code
+    fi
+
+    if [[ -f $decompressed_file ]]; then
+        rm $decompressed_file
+    fi
+
+    if [[ -f $compressed_file ]]; then
+        rm $compressed_file
+    fi
+}
+
 RED='\033[0;31m'
 BLUE='\033[0;36m'
 GREEN='\033[0;32m'
 NC='\033[0m' # no color
-declare -i exit_code=0
 test_file="./samples/bible.txt"
+compression_target=10
 
 while getopts ":hsfki:" option; do
     case $option in
@@ -32,6 +47,8 @@ while getopts ":hsfki:" option; do
             test_file=$OPTARG;;
         k)
             keep_file='yes';;
+        t)
+            compression_target=$OPTARG;;
         
         \?)
             echo "Error: Invalid option"
@@ -48,6 +65,7 @@ fi
 
 compressed_file="compressed-tmp-$(uuidgen).huffenc"
 decompressed_file="decompressed-tmp-$(uuidgen).txt"
+trap _cleanup EXIT
 
 compression_speed="$(time ( TIMEFORMAT='%lU'; ./hcode $mode $test_file $compressed_file ) 2>&1 1>/dev/null )"
 printf "Compression speed: ${BLUE} $compression_speed ${NC}\n"
@@ -55,26 +73,27 @@ printf "Compression speed: ${BLUE} $compression_speed ${NC}\n"
 decompression_speed="$(time ( TIMEFORMAT='%lU'; ./hcode -d $compressed_file $decompressed_file ) 2>&1 1>/dev/null )"
 printf "Decompression speed: ${BLUE} $decompression_speed ${NC}\n"
 
-decompressed_size="$(ls -lh $decompressed_file | awk '{print $5}')"
-compressed_size="$(ls -lh $compressed_file | awk '{print $5}')"
+decompressed_size_human_readable="$(ls -lh $decompressed_file | awk '{print $5}')"
+compressed_size_human_readable="$(ls -lh $compressed_file | awk '{print $5}')"
 
-printf "Decompressed size: ${BLUE} ${decompressed_size}B ${NC}\n"
-printf "Compressed size: ${BLUE} ${compressed_size}B ${NC}\n"
+printf "Decompressed size: ${BLUE} ${decompressed_size_human_readable}B ${NC}\n"
+printf "Compressed size: ${BLUE} ${compressed_size_human_readable}B ${NC}\n"
 
 cmp --silent $test_file $decompressed_file \
-&& printf "${GREEN}### PASSED: Decompressed file identical to source! ###\n${NC}" \
-|| (printf "${RED}### FAILED: Decompressed file different from source! ###\n${NC}" && declare -i exit_code=1)
+&& printf "${GREEN}### PASSED: Decompressed file identical to source file! ###\n${NC}" \
+|| { printf "${RED}### FAILED: Decompressed file different from source file! ###\n${NC}"; exit 1; }
 
-if [[ $keep_file == 'yes' ]]; then
-    exit $exit_code
+source_size=$(stat -c%s "$test_file")
+compressed_size=$(stat -c%s "$compressed_file")
+if [[  $compressed_size -gt $source_size ]]; then
+    printf "${RED}### FAILED: Compressed file bigger than source file! ###\n${NC}"; exit 1
+else
+    printf "${GREEN}### PASSED: Compressed file smaller than source file! ###\n${NC}"
 fi
 
-if [[ -f $decompressed_file ]]; then
-    rm $decompressed_file
+compression_percentage=$(bc <<< "scale=2; ($source_size - $compressed_size)/$source_size * 100")
+if (( $(echo "$compression_percentage < $compression_target" | bc -l) )); then
+    printf "${RED}### FAILED: compression of $compression_percentage %% is lower than the target of $compression_target %% ! ###\n${NC}"; exit 1
+else
+    printf "${GREEN}### PASSED: compression of $compression_percentage %% is higher than the target of $compression_target %% ! ###\n${NC}"
 fi
-
-if [[ -f $compressed_file ]]; then
-    rm $compressed_file
-fi
-
-exit $exit_code
